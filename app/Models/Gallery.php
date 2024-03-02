6774190fc376bcec978190271c9792350ad6a4c9
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-02 20:07:36 +0900

    各モデルにリレーションを定義した

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -6,22 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Like extends Model
+class Gallery extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'user_id',
-        'event_id'
+        'image',
+        'caption'
     ];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function event(): BelongsTo
-    {
-        return $this->belongsTo(Event::class);
     }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    // 多対多
+    public function events(): BelongsToMany
+    {
+        return $this->BelongsToMany(Event::class);
+    }
 }
